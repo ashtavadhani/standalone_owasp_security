a00d0b78291aef992228cd9e052329715fa22e55
Akshay Ashtavadhani <109575382+ashtavadhani@users.noreply.github.com>
2026-06-24 12:17:29 +0530

    Update vulnerable_code.py

--- src/vulnerable_code.py
+++ src/vulnerable_code.py
@@ -3,9 +3,9 @@ import sqlite3
 def get_user_data(username):
     # CRITICAL OWASP RISK: SQL Injection
     # Direct string formatting allows malicious actors to bypass login checks!
-    query = f"SELECT * FROM users WHERE username = '{username}'"
+    #query = f"SELECT * FROM users WHERE username = '{username}'"
     
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute(query)
+    #conn = sqlite3.connect("database.db")
+    #cursor = conn.cursor()
+    #cursor.execute(query)
     return cursor.fetchall()
